0d2f7feb827e46d414e8c72f35e911effba9bd5d
Levashov, Mykyta <mykyta.levashov@xa.epicgames.com>
2021-01-27 15:31:29 +0200

    Force elements solution

--- force-elems/src/Lib.hs
+++ force-elems/src/Lib.hs
@@ -3,4 +3,24 @@ module Lib
     ) where
 
 forceElems :: Traversable t => t a -> t a
-forceElems = undefined
+forceElems = runId . traverse pure
+
+data SId a = StrictId a | NonStrictId a
+
+runId :: SId a -> a
+runId (StrictId a) = a
+runId (NonStrictId a) = a
+
+instance Functor SId where
+    fmap f fa = NonStrictId $ case fa of
+        StrictId a -> f $! a
+        NonStrictId a -> f a
+
+instance Applicative SId where
+    pure a = StrictId a
+    ffa <*> fa = NonStrictId $ case (ffa, fa) of
+        (NonStrictId f, StrictId a) -> f $! a
+        (StrictId f, StrictId a) -> f $! a
+        (NonStrictId f, NonStrictId a) -> f a
+        (StrictId f, NonStrictId a) -> f a
+
